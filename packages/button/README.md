2397f3af6c6a12f5e8fa6c42c5b6078b1a741fde
绅士喵呜 <2234763217@qq.com>
2020-12-24 18:29:45 +0800

    build: init the lerna library

---
+++ packages/button/README.md
@@ -0,0 +1,11 @@
+# `@meow-ruler/react`
+
+> TODO: description
+
+## Usage
+
+```
+const react = require('@meow-ruler/react');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/input/src/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Input from '.'
+
+export default {
+  component: Input,
+  title: 'Design System/Input',
+}
+
+export const placeholder = () => <Input placeholder="user@gmail.com" />
